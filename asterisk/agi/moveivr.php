d553b72a104c5d90cdcb837c58de3e84d08aed52
Ha Truong <truongvieth@designveloper.com>
2014-09-19 16:20:09 +0700

    add API function control

--- asterisk/agi/moveivr.php
+++ asterisk/agi/moveivr.php
@@ -16,43 +16,44 @@ require "moveivr/astlib.class.php";
 define("DEBUG", true);
 define("TEST-MODE", true);
 
-define("TRIP-REVIEW-LIST-URL", "http://ivr.movetma.com/Movetivrapi/V1/GetTripReviewList");
-define("CANCEL-TRIP-LIST-URL", "http://ivr.movetma.com/Movetivrapi/V1/GetCancelTripList");
-define("CANCEL-TRIP-URL", "http://ivr.movetma.com/Movetivrapi/V1/SetTripCancel");
-define("CARPOOL-RENEWAL-LIST-URL", "http://ivr.movetma.com/Movetivrapi/V1/GetCarpoolRenewalList");
-define("CARPOOL-RENEWAL-URL", "http://ivr.movetma.com/Movetivrapi/V1/SetCarpoolRenewal");
-define("WHERE-IS-MY-RIDE-URL", "http://ivr.movetma.com/Movetivrapi/V1/GetWhereisMyRide");
+define("TRIP_REVIEW_LIST_URL", "http://ivr.movetma.com/Movetivrapi/V1/GetTripReviewList");
+define("CANCEL_TRIP_LIST_URL", "http://ivr.movetma.com/Movetivrapi/V1/GetCancelTripList");
+define("CANCEL_TRIP_URL", "http://ivr.movetma.com/Movetivrapi/V1/SetTripCancel");
+define("CARPOOL_RENEWAL_LIST_URL", "http://ivr.movetma.com/Movetivrapi/V1/GetCarpoolRenewalList");
+define("CARPOOL_RENEWAL_URL", "http://ivr.movetma.com/Movetivrapi/V1/SetCarpoolRenewal");
+define("WHERE_IS_MY_RIDE_URL", "http://ivr.movetma.com/Movetivrapi/V1/GetWhereisMyRide");
 
 
 
 // excu API GET request to server and parse JSON data response
 function getAPICaller($ast, $url) {
 
-  $ast->verbose("getAPICaller url: "$url);
-  
+	$ast->verbose("getAPICaller() with URL: " . $url);
+
 	//open connection
 	$ch = curl_init();
-	
+
 	//set the url
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
 
-  // Execute
-  $json_data = curl_exec($ch);
-  //$json_data = str_replace('"{',"{",$json_data);
-  //$json_data = str_replace('}"',"}",$json_data);
-  
-  //close connection
-  curl_close($ch);
+	// Execute
+	$json_data = curl_exec($ch);
+	//$json_data = str_replace('"{',"{",$json_data);
+	//$json_data = str_replace('}"',"}",$json_data);
 
-  // JSON parsing step
-  $order   = array("\r\n", "\n", "\r", "\\");
-  $replace = '';
-  $newstr = str_replace($order, $replace, $json_data);
-  $data = json_decode(''.$newstr);
+	//close connection
+	curl_close($ch);
+	// JSON parsing step
+	$order   = array("\r\n", "\n", "\r", "\\");
+	$replace = '';
+	$newstr = str_replace($order, $replace, $json_data);
+	$data = json_decode(''.$newstr);
 
-  return $data;
+	$responseStatus = $data->status;
+	$ast->verbose("getAPICaller() done with response status: " . $responseStatus);
+  	return $data;
 }
 
 // do authentication task
@@ -60,12 +61,12 @@ function userAuthentication($ast, $authInput){
 	if (DEBUG)
 		$ast->verbose("userAuthentication() Start");
 
-  // parsing from user input
+	// parsing from user input
 	$ast->set_variable("AUTH-PHONE", substr($authInput, 0, 10));
-  $ast->set_variable("AUTH-PIN", substr($authInput, -4));	
+	$ast->set_variable("AUTH-PIN", substr($authInput, -4));	
 
-  // for now always set valid authen data to Yes
-  $ast->set_variable("AUTH-VALID", "Y");
+	// for now always set valid authen data to Yes
+	$ast->set_variable("AUTH-VALID", "Y");
 
 	if (DEBUG)
 		$ast->verbose("userAuthentication() Stop");
@@ -76,16 +77,119 @@ function getTripReviewList($ast){
 	if (DEBUG)
 		$ast->verbose("getTripReviewList() Start");
 
-  $authPhone = $ast->get_variable("AUTH-PHONE");
-  $authPin = $ast->get_variable("AUTH-PIN");
-  $calleridNum = $ast->get_variable("CALLERID(num)");
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
 
-  $requestUrl = TRIP-REVIEW-LIST-URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
-  $responseData = getAPICaller($ast, $requestUrl);
+  	$requestUrl = TRIP_REVIEW_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+    $responseData = getAPICaller($ast, $requestUrl);
+
+    //print_r($responseData->result[0]->Tripid);
+    $totalTrip = count($responseData->result);
 
 	if (DEBUG)
-		$ast->verbose("getTripReviewList() Stop");
+		$ast->verbose("getTripReviewList() Stopped with $totalTrip trips to review.");
 }
+
+// get trip cancel list information
+function getTripCancelList($ast){
+	if (DEBUG)
+		$ast->verbose("getTripCancelList() Start");
+
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
+
+  	$requestUrl = CANCEL_TRIP_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+    $responseData = getAPICaller($ast, $requestUrl);
+
+    //print_r($responseData->result[0]->Tripid);
+    $totalTrip = count($responseData->result);
+
+	if (DEBUG)
+		$ast->verbose("getTripCancelList() Stopped with $totalTrip trips.");
+}
+
+// cancel trips
+function cancelTrips($ast){
+	if (DEBUG)
+		$ast->verbose("cancelTrips() Start");
+
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
+
+  	// two more fields required for this request (now is just test data)
+  	$tripIDList="T12345,T12346";
+  	$token="Movet_241";
+
+  	$requestUrl = CANCEL_TRIP_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum&tripidlist=$tripIDList&token=$token";
+    $responseData = getAPICaller($ast, $requestUrl);
+
+	if (DEBUG)
+		$ast->verbose("cancelTrips() done.");
+}
+
+
+// get carpool renewwal list information
+function getCarpoolRenewalList($ast){
+	if (DEBUG)
+		$ast->verbose("getCarpoolRenewalList() Start");
+
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
+
+  	$requestUrl = CARPOOL_RENEWAL_LIST_URL. "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+    $responseData = getAPICaller($ast, $requestUrl);
+
+    $totalItems = count($responseData->result);
+
+	if (DEBUG)
+		$ast->verbose("getCarpoolRenewalList() Stopped with $totalItems item.");
+}
+
+
+// set carpool renewwal
+function setCarpoolRenewwal($ast){
+	if (DEBUG)
+		$ast->verbose("setCarpoolRenewwal() Start");
+
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
+
+  	// more fields required for this request (now is just test data)
+  	$SoId="S124";
+  	$token="Movet_241";
+  	$DurationInMonths = "4";
+
+  	$additionalFields = "&SoId=$SoId&DurationInMonths=$DurationInMonths&token=$token";
+
+  	$requestUrl = CARPOOL_RENEWAL_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum" . $additionalFields;
+    $responseData = getAPICaller($ast, $requestUrl);
+
+	if (DEBUG)
+		$ast->verbose("setCarpoolRenewwal() done.");
+}
+
+
+// get whereIsMyRide information
+function whereIsMyRide($ast){
+	if (DEBUG)
+		$ast->verbose("whereIsMyRide() Start");
+
+  	$authPhone = $ast->get_variable("AUTH-PHONE");
+  	$authPin = $ast->get_variable("AUTH-PIN");
+  	$calleridNum = $ast->get_variable("CALLERID(num)");
+
+  	$requestUrl = WHERE_IS_MY_RIDE_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+    $responseData = getAPICaller($ast, $requestUrl);
+
+	if (DEBUG)
+		$ast->verbose("whereIsMyRide() Stopped");
+}
+
 
 /*----------------------------------------------------------
 	MOVEIVR main processing
@@ -99,13 +203,28 @@ function Main($ast, $db, $argv){
 	switch($command)
 	{
 		case "AUTH":
-		  $authInput = $ast->get_variable("AUTHINPUT");
+		  	$authInput = $ast->get_variable("AUTHINPUT");
 			userAuthentication($ast, $authInput);
 			break;
 		case "GET-TRIP-REVIEW-LIST":
-		  $authInput = $ast->get_variable("AUTHINPUT");
-			userAuthentication($ast, $authInput);
+		  	getTripReviewList($ast);
 			break;
+		case "GET-TRIP-CANCEL-LIST":
+		  	getTripCancelList($ast);
+			break;
+		case "CANCEL-TRIPS":
+		  	cancelTrips($ast);
+			break;
+		case "GET-CARPOOL-RENEWAL-LIST":
+		  	getCarpoolRenewalList($ast);
+			break;
+		case "SET-CARPOOL-RENEWAL":
+		  	setCarpoolRenewwal($ast);
+			break;
+		case "WHERE-IS-MY-RIDE":
+		  	whereIsMyRide($ast);
+			break;
+
 		default:
 			break;
 	}
