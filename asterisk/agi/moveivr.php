c113662303dc327be1fa1e2cfd805db353a07867
Ha Truong <truongvieth@designveloper.com>
2014-09-29 14:35:15 +0700

    update IVR for get trip review list

--- asterisk/agi/moveivr.php
+++ asterisk/agi/moveivr.php
@@ -33,22 +33,18 @@ function text2speech($filename, $text) {
 function getAPICaller($ast, $url) {
 
 	$ast->verbose("getAPICaller() with URL: " . $url);
-
-	//open connection
+	// open connection
 	$ch = curl_init();
 
-	//set the url
+	//C set the url
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-
-	// Execute
+	// Execute request 
 	$json_data = curl_exec($ch);
-	//$json_data = str_replace('"{',"{",$json_data);
-	//$json_data = str_replace('}"',"}",$json_data);
-
-	//close connection
+	// lose connection
 	curl_close($ch);
+
 	// JSON parsing step
 	$order   = array("\r\n", "\n", "\r", "\\");
 	$replace = '';
@@ -57,7 +53,8 @@ function getAPICaller($ast, $url) {
 
 	$responseStatus = $data->status;
 	$ast->verbose("getAPICaller() done with response status: " . $responseStatus);
-  	return $data;
+
+  return $data;
 }
 
 // do authentication task
@@ -81,45 +78,53 @@ function getTripReviewList($ast){
 	if (DEBUG)
 		$ast->verbose("getTripReviewList() Start");
 
-  	$authPhone = $ast->get_variable("AUTH-PHONE");
-  	$authPin = $ast->get_variable("AUTH-PIN");
-  	$calleridNum = $ast->get_variable("CALLERID(num)");
+	$authPhone = $ast->get_variable("AUTH-PHONE");
+	$authPin = $ast->get_variable("AUTH-PIN");
+	$calleridNum = $ast->get_variable("CALLERID(num)");
 
-  	$requestUrl = TRIP_REVIEW_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
-    $responseData = getAPICaller($ast, $requestUrl);
+	$requestUrl = TRIP_REVIEW_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+  $responseData = getAPICaller($ast, $requestUrl);
 
-    //print_r($responseData->result[0]->Tripid);
-    $totalTrip = count($responseData->result);
+  $totalTrip = count($responseData->result);
 
-    // Response to the caller: You have a trip going from < Pickup_Address > to < Dropoff_Address > 
-    // on <Travel_Date> provided by <Vendor>.  <Vendor> will pick you at < PickupTime> for you appointment at <DropoffTime>
-    $ast->exec("Festival","You have $totalTrip trip to review.");
-    $count_trip = 0;
-    
-    foreach ($responseData->result as $trip) {
-    	$count_trip++;
-    	$tripId = $trip->Tripid;
-    	$Pickup_Address = $trip->Pickup_Address;
-    	$Dropoff_Address = $trip->Dropoff_Address;
-    	$Travel_Date = $trip->Travel_Date;
-    	$Vendor = $trip->Vendor;
-    	$PickupTime = $trip->PickupTime;
-    	$DropOffTime = $trip->DropOffTime;
+  // Response to the caller: You have a trip going from < Pickup_Address > to < Dropoff_Address > 
+  // on <Travel_Date> provided by <Vendor>.  <Vendor> will pick you at < PickupTime> for you appointment at <DropoffTime>
+  //$ast->exec("Festival","You have $totalTrip trip to review.");
+  $callUID = $ast->get_variable("UNIQUEID");
+  $count_trip = 0;
+  $trip_list_text = "";
 
-    	$text = "Trip number $count_trip going from $Pickup_Address to $Dropoff_Address on ";
-    	$text2 = " provided by $Vendor.  $Vendor will pick you at $PickupTime  for you appointment at $DropOffTime";
+  foreach ($responseData->result as $trip) {
+  	$count_trip++;
+  	$tripId = $trip->Tripid;
+  	$Pickup_Address = $trip->Pickup_Address;
+  	$Dropoff_Address = $trip->Dropoff_Address;
+  	$Travel_Date = $trip->Travel_Date;
+  	$Vendor = $trip->Vendor;
+  	$PickupTime = $trip->PickupTime;
+  	$DropOffTime = $trip->DropOffTime;
 
- 		$order   = array(",", ";");
-		$replace = '';
-		$text = str_replace($order, $replace, $text); 
-		$text2 = str_replace($order, $replace, $text2); 
+  	$text = "Trip number $count_trip going from $Pickup_Address to $Dropoff_Address on $Travel_Date"
+  	         . " provided by $Vendor.  $Vendor will pick you at $PickupTime  for you appointment at $DropOffTime. ";
 
-		$ast->exec("Festival", $text);
-		$timestamp = strtotime("$Travel_Date");
-		$ast->say_date($timestamp);
-    	$ast->exec("Festival", $text2);
-    }
-    
+    /*
+		$order   = array(",", ";");
+  	$replace = '';
+  	$text = str_replace($order, $replace, $text); 
+  	$text2 = str_replace($order, $replace, $text2); 
+
+  	$ast->exec("Festival", $text);
+  	$timestamp = strtotime("$Travel_Date");
+  	$ast->say_date($timestamp);
+  	$ast->exec("Festival", $text2);
+    */
+    $trip_list_text = $trip_list_text . $text;
+  }
+
+  $filename = "TripReviewList-" . $callUID;
+  text2speech($filename, $trip_list_text);
+  $ast->set_variable("TRIP-REVIEW-LIST-AUDIO", "$filename");
+   
 	if (DEBUG)
 		$ast->verbose("getTripReviewList() Stopped with $totalTrip trips to review.");
 }
@@ -129,64 +134,52 @@ function getTripCancelList($ast){
 	if (DEBUG)
 		$ast->verbose("getTripCancelList() Start");
 
-  	$authPhone = $ast->get_variable("AUTH-PHONE");
-  	$authPin = $ast->get_variable("AUTH-PIN");
-  	$calleridNum = $ast->get_variable("CALLERID(num)");
+	$authPhone = $ast->get_variable("AUTH-PHONE");
+	$authPin = $ast->get_variable("AUTH-PIN");
+	$calleridNum = $ast->get_variable("CALLERID(num)");
 
-  	$requestUrl = CANCEL_TRIP_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
-    $responseData = getAPICaller($ast, $requestUrl);
+	$requestUrl = CANCEL_TRIP_LIST_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
+  $responseData = getAPICaller($ast, $requestUrl);
 
-    //print_r($responseData->result[0]->Tripid);
-    $totalTrip = count($responseData->result);
-    $callUID = $ast->get_variable("UNIQUEID");
+  //print_r($responseData->result[0]->Tripid);
+  $totalTrip = count($responseData->result);
+  $callUID = $ast->get_variable("UNIQUEID");
 
-    $count_trip = 0; 
-    $trip_list_text = "";
-    foreach ($responseData->result as $trip) {
-    	// Response to the user: Press (n) to cancel trip going from < Pickup_Address > to < Dropoff_Address> 
-		// on <Travel_Date> provided by <vendor> at < PickupTime>.
-		$count_trip++;
-    	$tripId = $trip->Tripid;
-    	$Pickup_Address = $trip->Pickup_Address;
-    	$Dropoff_Address = $trip->Dropoff_Address;
-    	$Travel_Date = $trip->Travel_Date;
-    	$Vendor = $trip->Vendor;
-    	$PickupTime = $trip->PickupTime;
+  $count_trip = 0; 
+  $trip_list_text = "";
 
-    	$text = "Press $count_trip to cancel trip going from $Pickup_Address to $Dropoff_Address on "
-    			. "$Travel_Date provided by $Vendor at $PickupTime.";
-    	
-    	$trip_list_text = $trip_list_text . $text;
-    	/*
-    	$text2 = "provided by $Vendor at $PickupTime.";
-    	$order   = array(",", ";");
-		$replace = '';
-		$text = str_replace($order, $replace, $text); 
-		$text2 = str_replace($order, $replace, $text2); 
-		$ast->exec("Festival", $text);
-		$timestamp = strtotime("$Travel_Date");
-		$ast->say_date($timestamp);
-    	$ast->exec("Festival", $text2);
-    	*/
+  foreach ($responseData->result as $trip) {
+  	// Response to the user: Press (n) to cancel trip going from < Pickup_Address > to < Dropoff_Address> 
+	  // on <Travel_Date> provided by <vendor> at < PickupTime>.
+	  $count_trip++;
+  	$tripId = $trip->Tripid;
+  	$Pickup_Address = $trip->Pickup_Address;
+  	$Dropoff_Address = $trip->Dropoff_Address;
+  	$Travel_Date = $trip->Travel_Date;
+  	$Vendor = $trip->Vendor;
+  	$PickupTime = $trip->PickupTime;
 
-    	// store TRIPTID so we can use it later
-    	$ast->set_variable("TRIP".$count_trip,$tripId);
-    }
-	$triplist_filename = "TripCancelList-" . $callUID;
-    text2speech($triplist_filename, $trip_list_text);
-    $ast->set_variable("TRIP-CANCEL-LIST-AUDIO", "$triplist_filename");
+  	$text = "Press $count_trip to cancel trip going from $Pickup_Address to $Dropoff_Address on "
+  			. "$Travel_Date provided by $Vendor at $PickupTime.";
+  	
+  	$trip_list_text = $trip_list_text . $text;
+  	// store TRIPTID so we can use it later
+  	$ast->set_variable("TRIP".$count_trip,$tripId);
+  }
 
-	// Would you like to hear this again? 
-	// Press (n+1) for yes 
-	// Press (n+2) for no 
-    $yes_key = $totalTrip + 1;
-    $no_key = $totalTrip + 2;
-    $confirm_text = "Would you like to hear this again? Press $yes_key for yes.  Press $no_key for no ";
-    $filename = "TripCancelList-HearAgain-" . $callUID;
-    text2speech($filename, $confirm_text);
-    $ast->set_variable("CANCEL-LIST-AGAIN-AUDIO", "$filename");
-    // set total trips value to keep track
-    $ast->set_variable("TOTAL-TRIP", "$totalTrip");
+  $triplist_filename = "TripCancelList-" . $callUID;
+  text2speech($triplist_filename, $trip_list_text);
+  $ast->set_variable("TRIP-CANCEL-LIST-AUDIO", "$triplist_filename");
+
+	// Would you like to hear this again? Press (n+1) for yes. Press (n+2) for no.
+  $yes_key = $totalTrip + 1;
+  $no_key = $totalTrip + 2;
+  $confirm_text = "Would you like to hear this again? Press $yes_key for yes.  Press $no_key for no ";
+  $filename = "TripCancelList-HearAgain-" . $callUID;
+  text2speech($filename, $confirm_text);
+  $ast->set_variable("CANCEL-LIST-AGAIN-AUDIO", "$filename");
+  // et total trips value to keep track
+  $ast->set_variable("TOTAL-TRIP", "$totalTrip");
 
 	if (DEBUG)
 		$ast->verbose("getTripCancelList() Stopped with $totalTrip trips.");
@@ -325,6 +318,16 @@ function whereIsMyRide($ast){
 
   	$requestUrl = WHERE_IS_MY_RIDE_URL . "?phone=$authPhone&code=$authPin&callerid=$calleridNum";
     $responseData = getAPICaller($ast, $requestUrl);
+
+    // check if request success to server
+    if ($responseData->status == "OK"){
+    	// Playback: Your vehicle will arrive at <Pickup_Address> at <ETA>
+    }
+    else {
+    	$message = $responseData->message;
+    	$ast->exec("Festival", "$message");
+    }
+    
 
 	if (DEBUG)
 		$ast->verbose("whereIsMyRide() Stopped");
